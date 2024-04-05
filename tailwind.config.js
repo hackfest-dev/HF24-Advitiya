e2b5e6cb2ffbe2235d0ef9d703df51830d7b6184
Aman Kumar Rai <tej171995@gmail.com>
2024-04-05 19:54:25 +0530

    Revert "second commit"

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode: 'jit', // Enable JIT mode
-  purge: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
-  darkMode: false, 
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Arimo', 'sans-serif'], 
-      },
-      fontWeight: {
-        normal: 400,
-        medium: 500,
-        semibold:600,
-        bold: 700,
-        
-      },
-      colors: {
-        'custom-green': '#173308',
-      },
-      gradientColorStops: theme => ({
-        'blue-start': '#8feafe',
-        'blue-end': '#a5ff6f',
-      }),
-    },
-  },
-  variants: {
     extend: {},
   },
-  plugins: [
-  ],
+  plugins: [],
 };
